7f0f1157ab94108a1bed1a91e416efc7042a9d71
Ling Jiang <jiangling91@outlook.com>
2018-12-10 23:58:10 +0800

    Update nnCostFunction.m

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -38,6 +38,33 @@ Theta2_grad = zeros(size(Theta2));
 %         variable J. After implementing Part 1, you can verify that your
 %         cost function computation is correct by verifying the cost
 %         computed in ex4.m
+
+% add bia units to X
+a1 = [ones(m,1) X];
+
+%compute a(2)
+z1 = Theta1 * a1';
+a2 = 1./(1+exp(-z1));
+
+% compute a(3)
+a2 = [ones(1,m);a2];
+z2 = Theta2 * a2;
+a3 = 1./(1+exp(-z2));
+
+%create matrix 10x5000 with each row an index of label
+identity = eye(num_labels);
+logicalY = identity(y, :); 
+
+%Unregulized cost function
+J_unreg=1./m*sum(diag(-logicalY*log(a3)-(1-logicalY)*log(1-a3)));
+
+%Adding regulation
+thetaone = Theta1(:,[2:end]);
+thetatwo = Theta2(:,[2:end]);
+Theta=[transpose(thetaone(:)) transpose(thetatwo(:))]
+reg=lambda./(2*m)*sum(Theta.^2)
+
+J=J_unreg+reg %with regulisation 
 %
 % Part 2: Implement the backpropagation algorithm to compute the gradients
 %         Theta1_grad and Theta2_grad. You should return the partial derivatives of
@@ -53,6 +80,19 @@ Theta2_grad = zeros(size(Theta2));
 %         Hint: We recommend implementing backpropagation using a for-loop
 %               over the training examples if you are implementing it for the 
 %               first time.
+
+%compute sigma3 and sigma2
+sigma3 = a3 - logicalY';
+g = a2.*(1-a2);
+sigma2 = (Theta2' * sigma3).* g;
+
+%compute delta2 and delta1
+delta2 = sigma3 * a2';
+delta1 = sigma2(2:end,:) * a1
+
+Theta2_gradunreg=1./m*delta2
+Theta1_gradunreg=1./m*delta1
+
 %
 % Part 3: Implement regularization with the cost function and gradients.
 %
@@ -61,18 +101,13 @@ Theta2_grad = zeros(size(Theta2));
 %               the regularization separately and then add them to Theta1_grad
 %               and Theta2_grad from Part 2.
 %
-
-
-
-
-
-
-
-
-
-
-
-
+T2 = Theta2
+size(T2(:,1))
+T2(:,1) = zeros(num_labels,1)
+T1 = Theta1
+T1(:,1)=zeros(hidden_layer_size,1)
+Theta2_grad=Theta2_gradunreg+lambda./m*T2
+Theta1_grad=Theta1_gradunreg+lambda./m*T1
 
 
 
